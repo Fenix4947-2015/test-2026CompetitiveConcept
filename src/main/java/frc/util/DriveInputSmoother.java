48326feff134914573ba3067b33c8a8c61c81df6
antoine-l <lecielestbleu@hotmail.com>
2026-02-19 20:21:18 -0500

    Refactor DriveInputSmoother to use SlewRateLimiters for smoother input handling

--- src/main/java/frc/util/DriveInputSmoother.java
+++ src/main/java/frc/util/DriveInputSmoother.java
@@ -3,17 +3,21 @@ package frc.util;
 import java.util.function.DoubleSupplier;
 
 import edu.wpi.first.math.MathUtil;
-import edu.wpi.first.math.VecBuilder;
-import edu.wpi.first.math.Vector;
-import edu.wpi.first.math.numbers.N2;
+import edu.wpi.first.math.filter.SlewRateLimiter;
 
 public class DriveInputSmoother {
     private static final double kJoystickDeadband = 0.15;
     private static final double kCurveExponent = 1.5;
+    private static final double kTranslationSlewRate = 3.0; // units per second
+    private static final double kRotationSlewRate = 3.0;    // units per second
 
     private final DoubleSupplier forwardInput;
     private final DoubleSupplier leftInput;
     private final DoubleSupplier rotationInput;
+
+    private final SlewRateLimiter forwardLimiter = new SlewRateLimiter(kTranslationSlewRate);
+    private final SlewRateLimiter leftLimiter = new SlewRateLimiter(kTranslationSlewRate);
+    private final SlewRateLimiter rotationLimiter = new SlewRateLimiter(kRotationSlewRate);
 
     public DriveInputSmoother(DoubleSupplier forwardInput, DoubleSupplier leftInput, DoubleSupplier rotationInput) {
         this.forwardInput = forwardInput;
@@ -26,18 +30,26 @@ public class DriveInputSmoother {
     }
 
     public ManualDriveInput getSmoothedInput() { 
-        final Vector<N2> rawTranslationInput = VecBuilder.fill(forwardInput.getAsDouble(), leftInput.getAsDouble());
-        final Vector<N2> deadbandedTranslationInput = MathUtil.applyDeadband(rawTranslationInput, kJoystickDeadband);
-        final Vector<N2> curvedTranslationInput = MathUtil.copyDirectionPow(deadbandedTranslationInput, kCurveExponent);
+        final double rawForward = forwardInput.getAsDouble();
+        final double rawLeft = leftInput.getAsDouble();
+        final double rawRotation = rotationInput.getAsDouble();
 
-        final double rawRotationInput = rotationInput.getAsDouble();
-        final double deadbandedRotationInput = MathUtil.applyDeadband(rawRotationInput, kJoystickDeadband);
-        final double curvedRotationInput = MathUtil.copyDirectionPow(deadbandedRotationInput, kCurveExponent);
+        final double deadbandedForward = MathUtil.applyDeadband(rawForward, kJoystickDeadband);
+        final double deadbandedLeft = MathUtil.applyDeadband(rawLeft, kJoystickDeadband);
+        final double deadbandedRotation = MathUtil.applyDeadband(rawRotation, kJoystickDeadband);
+
+        final double curvedForward = MathUtil.copyDirectionPow(deadbandedForward, kCurveExponent);
+        final double curvedLeft = MathUtil.copyDirectionPow(deadbandedLeft, kCurveExponent);
+        final double curvedRotation = MathUtil.copyDirectionPow(deadbandedRotation, kCurveExponent);
+
+        final double limitedForward = forwardLimiter.calculate(curvedForward);
+        final double limitedLeft = leftLimiter.calculate(curvedLeft);
+        final double limitedRotation = rotationLimiter.calculate(curvedRotation);
 
         return new ManualDriveInput(
-            curvedTranslationInput.get(0), 
-            curvedTranslationInput.get(1), 
-            curvedRotationInput
+            limitedForward, 
+            limitedLeft, 
+            limitedRotation
         );
     }
 }
