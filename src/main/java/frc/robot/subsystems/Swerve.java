fd0f5b5a4aebc1d5319ae1f873131fa261e7f18c
antoine-l <lecielestbleu@hotmail.com>
2026-02-24 19:44:56 -0500

    force pas de rotation de path

--- src/main/java/frc/robot/subsystems/Swerve.java
+++ src/main/java/frc/robot/subsystems/Swerve.java
@@ -82,15 +82,15 @@ public class Swerve extends TunerSwerveDrivetrain implements Subsystem {
             ),
             config, // The robot configuration
             () -> {
-            // Boolean supplier that controls when the path will be mirrored for the red alliance
-            // This will flip the path being followed to the red side of the field.
-            // THE ORIGIN WILL REMAIN ON THE BLUE SIDE
-
-            var alliance = DriverStation.getAlliance();
-            if (alliance.isPresent()) {
-                return alliance.get() == DriverStation.Alliance.Red;
-            }
-            return false;
+                // Boolean supplier that controls when the path will be mirrored for the red alliance
+                // This will flip the path being followed to the red side of the field.
+                // THE ORIGIN WILL REMAIN ON THE BLUE SIDE
+                //TODO remettre logique et faire path dans zone bleu
+                // var alliance = DriverStation.getAlliance();
+                // if (alliance.isPresent()) {
+                //     return alliance.get() == DriverStation.Alliance.Red;
+                // }
+                return false;
             },
             this // Reference to this subsystem to set requirements
         );
